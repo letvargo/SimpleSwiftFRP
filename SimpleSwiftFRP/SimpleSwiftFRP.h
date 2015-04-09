5e508288f57811c6c0e7dbd4165711dfc1c9066a
letvargo <letvargo@gmail.com>
2015-04-08 20:02:04 -0700

    Initial Commit

---
+++ SimpleSwiftFRP/SimpleSwiftFRP.h
@@ -0,0 +1,19 @@
+//
+//  SimpleSwiftFRP.h
+//  SimpleSwiftFRP
+//
+//  Created by developer on 4/8/15.
+//  Copyright (c) 2015 letvargo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SimpleSwiftFRP.
+FOUNDATION_EXPORT double SimpleSwiftFRPVersionNumber;
+
+//! Project version string for SimpleSwiftFRP.
+FOUNDATION_EXPORT const unsigned char SimpleSwiftFRPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleSwiftFRP/PublicHeader.h>
+
+
